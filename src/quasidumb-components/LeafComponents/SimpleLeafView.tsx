1ae1c28f975f914ec4bd3fc3ea7f6709f97ef048
Liya Milenkova <liyakm@gmail.com>
2022-10-11 18:12:40 +0300

    Add: Simple Leaf View

---
+++ src/quasidumb-components/LeafComponents/SimpleLeafView.tsx
@@ -0,0 +1,38 @@
+import { Box, Paper, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { darkTheme, lightTheme, txtTheme } from '../../common/theme-colours';
+import AppState from '../../providers/app-state';
+import { ILeaf } from '../../services/leaf-service';
+import PushPinIcon from '@mui/icons-material/PushPin';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+interface SimpleLeafProps {
+    leaf: ILeaf;
+}
+export default function SimpleLeafView ({ leaf }: SimpleLeafProps) {
+  const { context: { theme } } = useContext(AppState);
+  return (
+    <Paper sx={{ 
+      bgcolor: theme !=='dark'? lightTheme.bgcolour:darkTheme.bgcolour, 
+      color: theme !=='dark'? lightTheme.txcolour:darkTheme.txcolour,
+      fontFamily: txtTheme.font,
+      p: 3,
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      gap: 2,
+      border: `2px solid ${theme ==='dark'? darkTheme.navcolour:darkTheme.bgcolour}`,
+      boxShadow: `5px 5px ${theme ==='dark'? `${darkTheme.navcolour}80`:'#00000090'}`
+    }}>
+      {leaf.title?<Typography variant='h6'>{leaf.title}</Typography> : null}
+      <Typography variant='body1'>{leaf.preview}...</Typography>
+      <Typography variant='caption'>{leaf.createdOn}</Typography>
+      <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <PushPinIcon sx={{ cursor: 'pointer', '&:hover': { color: lightTheme.accent } }}></PushPinIcon>
+        <EditIcon sx={{ cursor: 'pointer', '&:hover': { color: lightTheme.accent } }}/>
+        <DeleteIcon sx={{ cursor: 'pointer', '&:hover': { color: lightTheme.accent } }}/>
+      </Box>
+    </Paper>
+  );
+}
--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -1,14 +1,46 @@
 import Masonry from '@mui/lab/Masonry';
-import { Box, Button } from '@mui/material';
+import { Box, Button, Paper } from '@mui/material';
+import { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { darkTheme, lightTheme, txtTheme } from '../../common/theme-colours';
+import AppState from '../../providers/app-state';
+import SimpleLeafView from '../../quasidumb-components/LeafComponents/SimpleLeafView';
+import { getLeaves } from '../../services/leaf-service';
+
 
 export default function Dashboard () {
+
+  const { context: { user, theme } } = useContext(AppState);
+
+  const [leaves, setLeaves] = useState([]);
+
+  useEffect(() => {    
+    (async () => {
+      setLeaves(await getLeaves(user));
+    })();
+  }, []);
+  console.log(leaves);
   return (
-    <Box sx={{ width: '80vw', minHeight: 393 }}>
+    <Box sx={{ width: '80vw', minHeight: 393, boxSizing: 'border-box', my: 4 }}>
       <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={2}>
-        <Button component={Link} to={'/create-leaf'}>Create new leaf</Button>
       </Masonry>
       <Masonry columns={{ xs: 1, sm: 2, md: 3, lg: 4 }} spacing={2}>
+        <Paper sx={{ 
+          bgcolor: theme !=='dark'? lightTheme.bgcolour:darkTheme.bgcolour, 
+          color: theme !=='dark'? lightTheme.txcolour:darkTheme.txcolour,
+          fontFamily: txtTheme.font,
+          p: 3,
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'wrap',
+          gap: 2,
+          border: `2px solid ${theme ==='dark'? darkTheme.navcolour:darkTheme.bgcolour}`,
+          boxShadow: `5px 5px ${theme ==='dark'? `${darkTheme.navcolour}80`:'#00000090'}` 
+        }}
+        component={Link} to={'/create-leaf'}>
+          <Button component={Link} to={'/create-leaf'}>Create new leaf</Button>
+        </Paper>
+        {leaves.length? leaves.map((leaf) => <SimpleLeafView leaf={leaf}></SimpleLeafView>): <></>}
       </Masonry>
     </Box>
   );
